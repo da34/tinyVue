e316c44d00f29905454d262d980d63e6ce89d1d8
da34 <1255029618@qq.com>
2022-12-19 11:59:37 +0800

    feat: 实现 isProxy

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -20,6 +20,10 @@ export function isReadonly(value) {
     return !!value[ReactiveFlags.IS_READONLY]
 }
 
+export function isProxy(value) {
+    return isReactive(value) || isReadonly(value)
+}
+
 export function shallowReadonly(raw) {
     return createActiveObj(raw, shallowReadonlyHandles)
 }
